452130b0f14d7375659e09fd8b333062b0578b23
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-12-07 14:16:04 -0300

    Configuração inicial da camada de serviços com injeção de dependência
    Signed-off-by: Lucas Carvalho <lucas.carvalho7933@gmail.com>

---
+++ src/main/java/com/lucas/tarefas/services/CategoriaService.java
@@ -0,0 +1,14 @@
+package com.lucas.tarefas.services;
+
+import com.lucas.tarefas.Repository.CategoriaRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CategoriaService {
+
+    private final CategoriaRepository categoriaRepository;
+
+    public CategoriaService(CategoriaRepository categoriaRepository) {
+        this.categoriaRepository = categoriaRepository;
+    }
+}
---
+++ src/main/java/com/lucas/tarefas/services/UsuarioService.java
@@ -0,0 +1,14 @@
+package com.lucas.tarefas.services;
+
+import com.lucas.tarefas.Repository.UsuarioRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+}
