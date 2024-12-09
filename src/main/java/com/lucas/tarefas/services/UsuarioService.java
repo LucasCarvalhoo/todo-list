6b4200340c9bd5dc844e442c90bb534c73b88b5b
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-12-09 20:05:24 -0300

    update

--- src/main/java/com/lucas/tarefas/services/UsuarioService.java
+++ src/main/java/com/lucas/tarefas/services/UsuarioService.java
@@ -1,7 +1,14 @@
 package com.lucas.tarefas.services;
 
 import com.lucas.tarefas.Repository.UsuarioRepository;
+import com.lucas.tarefas.entities.Usuario;
+import jakarta.validation.constraints.Positive;
+import org.antlr.v4.runtime.misc.NotNull;
 import org.springframework.stereotype.Service;
+import jakarta.validation.Valid;
+
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UsuarioService {
@@ -11,4 +18,17 @@ public class UsuarioService {
     public UsuarioService(UsuarioRepository usuarioRepository) {
         this.usuarioRepository = usuarioRepository;
     }
+
+    Optional<Usuario> findById(@Positive Long id){
+        return usuarioRepository.findById(id);
+    }
+
+    List<Usuario> findByName(@Positive String nome){
+        return usuarioRepository.findByName(nome);
+    }
+
+    public Usuario criarUsuario(@NotNull @Valid Usuario usuario){
+        return usuarioRepository.save(usuario);
+    }
+
 }
