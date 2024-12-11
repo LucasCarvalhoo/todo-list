bfc28428d3769e648545059f50b980f18a983657
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-12-11 10:54:40 -0300

    buscar categorias por usuario

--- src/main/java/com/lucas/tarefas/Repository/CategoriaRepository.java
+++ src/main/java/com/lucas/tarefas/Repository/CategoriaRepository.java
@@ -4,6 +4,10 @@ import com.lucas.tarefas.entities.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    List<Categoria> findByUsuarioId(Long usuarioId);
 }
