c9c51df451982e98dbbda6ae21c5cd9f602cee55
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-12-11 10:53:43 -0300

    implementar regra de negocio da categoria

--- src/main/java/com/lucas/tarefas/services/CategoriaService.java
+++ src/main/java/com/lucas/tarefas/services/CategoriaService.java
@@ -1,21 +1,61 @@
 package com.lucas.tarefas.services;
 
 import com.lucas.tarefas.Repository.CategoriaRepository;
+import com.lucas.tarefas.Repository.UsuarioRepository;
 import com.lucas.tarefas.entities.Categoria;
 import com.lucas.tarefas.entities.Usuario;
+import com.lucas.tarefas.exception.CategoriaNaoCriadaException;
+import com.lucas.tarefas.exception.CategoriaNaoEncontradaException;
+import com.lucas.tarefas.exception.UsuarioNaoEncontradoException;
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Positive;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Service
 public class CategoriaService {
 
     private final CategoriaRepository categoriaRepository;
+    private final UsuarioRepository usuarioRepository;
 
-    public CategoriaService(CategoriaRepository categoriaRepository) {
+    public CategoriaService(CategoriaRepository categoriaRepository, UsuarioRepository usuarioRepository) {
         this.categoriaRepository = categoriaRepository;
+        this.usuarioRepository = usuarioRepository;
     }
 
-//    public Categoria criarCategoria(@Valid Categoria categoria){
-//        return categoriaRepository.save(categoria);
-//    }
+    public List<Categoria> list(){
+        return categoriaRepository.findAll();
+    }
+
+    public List<Categoria> listarCategoriaPorUsuario(Long usuarioId){
+        return categoriaRepository.findByUsuarioId(usuarioId);
+    }
+
+    public Categoria criarCategoria(Categoria categoria, Long usuarioId){
+        if (categoria.getNome() == null) {
+            throw new CategoriaNaoCriadaException("Nome da categoria está nulo.");
+        }
+        Usuario usuario = usuarioRepository.findById(usuarioId)
+                .orElseThrow(() -> new RuntimeException("Usuário não encontrado."));
+
+        categoria.setUsuario(usuario);
+        usuario.getCategorias().add(categoria);
+        return categoriaRepository.save(categoria);
+    }
+
+    public Categoria utualizarCategorias(Categoria categoria, Long categoriaId){
+        return categoriaRepository.findById(categoriaId)
+                .map(recordFound -> {
+                    recordFound.setNome(categoria.getNome());
+                    return categoriaRepository.save(recordFound);
+                }).orElseThrow(() -> new CategoriaNaoEncontradaException("Categoria não encontrada"));
+    }
+
+    public void deletarCategorias(Long categoriaId){
+        categoriaRepository.delete(categoriaRepository.findById(categoriaId)
+                .orElseThrow(() -> new CategoriaNaoEncontradaException("Categoria não encontrada: " + categoriaId)));
+    }
 }
