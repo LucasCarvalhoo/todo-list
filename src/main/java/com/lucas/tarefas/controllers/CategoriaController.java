65dea842fa146c49730e05e3295e5ad5e7985429
Lucas Carvalho <lucas.carvalho7933@gmail.com>
2024-12-23 11:44:47 -0300

    update
    Signed-off-by: Lucas Carvalho <lucas.carvalho7933@gmail.com>

--- src/main/java/com/lucas/tarefas/controllers/CategoriaController.java
+++ src/main/java/com/lucas/tarefas/controllers/CategoriaController.java
@@ -2,13 +2,10 @@ package com.lucas.tarefas.controllers;
 
 import com.lucas.tarefas.entities.Categoria;
 import com.lucas.tarefas.entities.Usuario;
-import com.lucas.tarefas.exception.CategoriaNaoCriadaException;
 import com.lucas.tarefas.services.CategoriaService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,13 +21,11 @@ public class CategoriaController {
     }
 
     @GetMapping
-    @PreAuthorize("hasRole('ADMIN')")
     public List<Categoria> list(){
         return categoriaService.list();
     }
 
     @GetMapping("/usuarios/{usuarioId}")
-    @PreAuthorize("#usuarioId == principal.id")
     public ResponseEntity<List<Categoria>> listarCategorias(@PathVariable Long usuarioId, @AuthenticationPrincipal Usuario usuarioLogado){
         List<Categoria> categorias = categoriaService.listarCategoriaPorUsuario(usuarioId, usuarioLogado);
         return ResponseEntity.ok(categorias);
@@ -38,19 +33,16 @@ public class CategoriaController {
 
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    @PreAuthorize("#usuarioId == principal.id")
     public Categoria criarCategorias(@RequestBody Categoria categoria, @RequestParam Long usuarioId, @AuthenticationPrincipal Usuario usuarioLogado){
         return categoriaService.criarCategoria(categoria, usuarioId, usuarioLogado);
     }
 
     @PutMapping
-    @PreAuthorize("#usuarioId == principal.id")
     public Categoria atualizarCategorias(@RequestBody Categoria categoria, @RequestParam Long usuarioId, @RequestParam Long categoriaId, @AuthenticationPrincipal Usuario usuarioLogado){
         return categoriaService.utualizarCategorias(categoria, usuarioId, categoriaId, usuarioLogado);
     }
 
     @DeleteMapping("/{id}")
-    @PreAuthorize("#usuarioId == principal.id")
     public void deletarCategorias(@PathVariable Long id){
         categoriaService.deletarCategorias(id);
     }
--- src/main/java/com/lucas/tarefas/controllers/TarefaController.java
+++ src/main/java/com/lucas/tarefas/controllers/TarefaController.java
@@ -2,9 +2,7 @@ package com.lucas.tarefas.controllers;
 
 import com.lucas.tarefas.Repository.UsuarioRepository;
 import com.lucas.tarefas.entities.Tarefa;
-import com.lucas.tarefas.entities.Usuario;
 import com.lucas.tarefas.services.TarefaService;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
@@ -26,38 +24,32 @@ public class TarefaController {
     }
 
     @GetMapping
-    @PreAuthorize("hasRole('ADMIN'")
     public List<Tarefa> listarTarefas(){
         return tarefaService.list();
     }
 
     @GetMapping("/usuario/{usuarioId}")
-    @PreAuthorize("hasRole('ADMIN' or #usuarioId == principal.id")
     public List<Tarefa> listarTarefasPorUsuario(@PathVariable Long usuarioId){
         return tarefaService.listarTarefasPorUsuario(usuarioId);
     }
 
     @GetMapping("/{id}")
-    @PreAuthorize("hasRole('ADMIN' or @tarefaService.isOwner(#tarefaId, principal.id)")
     public Optional<Tarefa> buscarTarefaPorId(@PathVariable Long tarefaId){
         return tarefaService.buscarTarefaPorId(tarefaId);
     }
 
     @PostMapping
-    @PreAuthorize("hasRole('ADMIN') or #usuarioId == principal.id")
     public Tarefa salvarTarefa(@RequestBody Tarefa tarefa, @RequestParam Long usuarioId, @RequestParam Long categoriaId, @AuthenticationPrincipal UserDetails userDetails){
         Long usuarioAutenticadoId = Long.parseLong(userDetails.getUsername());
         return tarefaService.salvarTarefa(tarefa, usuarioId, categoriaId, usuarioAutenticadoId);
     }
 
     @PutMapping("/{tarefaId}")
-    @PreAuthorize("hasRole('ADMIN' or @tarefaService.isOwner(#tarefaId, principal.id)")
     public Tarefa atualizarTarefas(@RequestBody Tarefa tarefa, @PathVariable Long tarefaId){
         return tarefaService.atualizarTarefa(tarefa, tarefaId);
     }
 
     @DeleteMapping("/{tarefaId}")
-    @PreAuthorize("hasRole('ADMIN' or @tarefaService.isOwner(#tarefaId, principal.id)")
     public void deletarTarefas(@PathVariable Long tarefaId){
         tarefaService.deletarTarefa(tarefaId);
     }
